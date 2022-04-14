cddb7aa37e20c3e3a4a2dd7bccaa247c0b10f0c3
priyawadhwa <priya_wadhwa@berkeley.edu>
2022-04-14 10:57:24 -0700

    Add in initial terraform scripts & Github Actions workflow (#3)
    * Initial workflows for deploying terraform and initial terraform
    
    * Run license fixer

---
+++ terraform/production/general.auto.tfvars
@@ -0,0 +1,2 @@
+project_id = "oci-signer-service-dev"
+region     = "us-central1"
---
+++ terraform/production/variables.tf
@@ -0,0 +1,57 @@
+variable "project_id" {
+  type = string
+  validation {
+    condition     = length(var.project_id) > 0
+    error_message = "Must specify project_id variable."
+  }
+}
+
+variable "ca_pool_name" {
+  description = "Certificate authority pool name"
+  type        = string
+  default     = "sigstore"
+}
+
+variable "monitoring" {
+  description = "Monitoring and alerting"
+  type = object({
+    enabled                 = bool
+    fulcio_url              = string
+    rekor_url               = string
+    dex_url                 = string
+    notification_channel_id = string
+  })
+  default = {
+    enabled                 = false
+    fulcio_url              = "fulcio.example.com"
+    dex_url                 = "oauth2.example.com"
+    rekor_url               = "rekor.example.com"
+    notification_channel_id = ""
+  }
+}
+
+// Optional values that can be overridden or appended to if desired.
+variable "cluster_name" {
+  description = "The name to give the new Kubernetes cluster."
+  type        = string
+  default     = "sigstore-prod"
+}
+
+variable "region" {
+  description = "The region in which to create the VPC network"
+  type        = string
+}
+
+// TODO
+# variable "github_repo" {
+#   description = "Github repo for running Github Actions from."
+#   type        = string
+#   default     = "sigstore/public-good-instance"
+# }
+
+variable "tunnel_accessor_sa" {
+  type        = string
+  description = "Email of group to give access to the tunnel to"
+  // TODO
+  default     = "user:priya@chainguard.dev"
+}
