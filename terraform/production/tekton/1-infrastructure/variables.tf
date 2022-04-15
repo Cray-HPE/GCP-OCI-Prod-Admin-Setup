564aa9fa74936be1e884f427c86acdf479af3b15
James Strong <strong.james.e@gmail.com>
2022-04-15 08:58:50 -0400

    reorder for fulcio and tekton deploys
    Signed-off-by: James Strong <strong.james.e@gmail.com>

---
+++ terraform/production/tekton/1-infrastructure/variables.tf
@@ -0,0 +1,35 @@
+variable "project_id" {
+  type = string
+  validation {
+    condition     = length(var.project_id) > 0
+    error_message = "Must specify project_id variable."
+  }
+}
+
+
+// Optional values that can be overridden or appended to if desired.
+variable "cluster_name" {
+  description = "The name to give the new Kubernetes cluster."
+  type        = string
+  default     = "tekton-prod"
+}
+
+variable "region" {
+  description = "The region in which to create the VPC network"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "Github repo for running Github Actions from."
+  type        = string
+  default     = "Cray-HPE/GCP-OCI-Prod-Admin-Setup"
+}
+
+// We don't actually need this but it's required by the bastion module
+// So just assign it to the github-actions SA, which already has the permissions that will be granted
+// in the bastion module
+variable "tunnel_accessor_sa" {
+  type        = string
+  description = "Email of group to give access to the tunnel to"
+  default     = "serviceAccount:github-actions@oci-ci-service-dev.iam.gserviceaccount.com"
+}
