10c9e3da0e3519fbb4ba29953f8be6fa4ea6c1d7
James Strong <strong.james.e@gmail.com>
2022-04-20 15:27:09 -0400

    remove network and move to primary
    Signed-off-by: James Strong <strong.james.e@gmail.com>

--- terraform/production/tekton/1-infrastructure/cluster.tf
+++ terraform/production/tekton/1-infrastructure/cluster.tf
@@ -1,13 +1,16 @@
-// TODO: Pin source to commits or releases
+variable "network_name" {
+  default = "oci-signer-service"
+  type = string
+  description = "Name of the network to deploy too"
+}
 
-// Private network
-module "network" {
-  source = "git::https://github.com/sigstore/scaffolding.git//terraform/gcp/modules/network"
-
-  region     = var.region
-  project_id = var.project_id
-
-  cluster_name = var.cluster_name
+variable "subnetwork_name" {
+  default = "primary-us-central-signer"
+  type = string
+  description = "Name of the subnetwork to deploy too"
+}
+data "google_compute_network" "primary" {
+  name = var.network_name
 }
 
 // Bastion
@@ -16,13 +19,9 @@ module "bastion" {
 
   project_id         = var.project_id
   region             = var.region
-  network            = module.network.network_name
-  subnetwork         = module.network.subnetwork_self_link
+  network            = var.network_name
+  subnetwork         = var.subnetwork_name
   tunnel_accessor_sa = var.tunnel_accessor_sa
-
-  depends_on = [
-    module.network
-  ]
 }
 
 module "cluster" {
@@ -31,21 +30,53 @@ module "cluster" {
 
   region     = var.region
   project_id = var.project_id
-
+  node_pool_name = var.cluster_name
   cluster_name = var.cluster_name
+  initial_node_count = 3
+  autoscaling_max_node = 10
 
-  network                       = module.network.network_self_link
-  subnetwork                    = module.network.subnetwork_self_link
-  cluster_secondary_range_name  = module.network.secondary_ip_range.0.range_name
-  services_secondary_range_name = module.network.secondary_ip_range.1.range_name
+
+  network                       = var.network_name
+  subnetwork                    = var.subnetwork_name
+  master_ipv4_cidr_block = var.master_ipv4_cidr_block
+  cluster_secondary_range_name  = "pod-range"
+  services_secondary_range_name = "svc-range"
 
   bastion_ip_address = module.bastion.ip_address
 
   depends_on = [
-    module.network,
     module.bastion
   ]
 }
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+variable "master_ipv4_cidr_block" {
+  type    = string
+  default = "172.16.0.16/28"
+}
+
+//needed till https://github.com/sigstore/scaffolding/pull/123/files is merged
+resource "google_compute_firewall" "master-webhooks" {
+  name      = "gke-${var.cluster_name}-webhooks-${random_id.suffix.hex}"
+  project   = var.project_id
+  network   = var.network_name
+  direction = "INGRESS"
+
+
+  source_ranges = [var.master_ipv4_cidr_block]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8443"]
+  }
+
+  //target_tags = [local.cluster_network_tag]
+
+  depends_on = [module.cluster]
+}
+
 
 // Cluster policies setup.
 module "policy_bindings" {
@@ -56,8 +87,4 @@ module "policy_bindings" {
 
   cluster_name = var.cluster_name
   github_repo  = var.github_repo
-
-  depends_on = [
-    module.network
-  ]
 }
