9030f35e45184c0fb0a9106166cb2dd46d463dbc
Priya Wadhwa <priya@chainguard.dev>
2022-04-19 20:48:15 -0400

    Release fulcio via terraform using GCP CA service as root

--- terraform/production/signer/3-sigstore-helm/provider.tf
+++ terraform/production/signer/3-sigstore-helm/provider.tf
@@ -107,3 +107,104 @@ resource "helm_release" "trillian" {
     EOF
   ]
 }
+
+
+resource "helm_release" "fulcio" {
+  name             = "fulcio"
+  repository       = "https://sigstore.github.io/helm-charts"
+  chart            = "fulcio"
+  namespace        = "fulcio-system"
+  create_namespace = true
+  atomic           = true
+  version          = "0.2.11"
+
+  depends_on = [
+    helm_release.trillian
+  ]
+
+  values = [
+    <<EOF
+    enabled: true
+    namespace:
+      name: fulcio-system
+      create: false
+    forceNamespace: fulcio-system
+    createcerts:
+      fullnameOverride: fulcio-createcerts
+    ctlog:
+      enabled: false
+    config:
+      contents: {
+        # TODO: Figure out which OIDC issuers, if any, are allowed
+        "OIDCIssuers": {
+          "https://accounts.google.com": {
+            "IssuerURL": "https://accounts.google.com",
+            "ClientID": "sigstore",
+            "Type": "email"
+          },
+          "https://oauth2.sigstore.dev/auth": {
+            "IssuerURL": "https://oauth2.sigstore.dev/auth",
+            "ClientID": "sigstore",
+            "Type": "email",
+            "IssuerClaim": "$.federated_claims.connector_id"
+          },
+          "https://token.actions.githubusercontent.com": {
+            "IssuerURL": "https://token.actions.githubusercontent.com",
+            "ClientID": "sigstore",
+            "Type": "github-workflow"
+          }
+        },
+        "MetaIssuers": {
+          "https://container.googleapis.com/v1/projects/*/locations/*/clusters/*": {
+            "ClientID": "sigstore",
+            "Type": "kubernetes"
+          },
+          "https://oidc.eks.*.amazonaws.com/id/*": {
+            "ClientID": "sigstore",
+            "Type": "kubernetes"
+          },
+          "https://oidc.prod-aks.azure.com/*": {
+            "ClientID": "sigstore",
+            "Type": "kubernetes"
+          }
+        }
+      }
+    server:
+      fullnameOverride: fulcio-server
+      replicaCount: 3
+      podAnnotations:
+        prometheus.io/scrape: "true"
+        prometheus.io/path: "/metrics"
+        prometheus.io/port: "2112"
+      ingress:
+        //TODO (priyawadhwa): Update when we have ingress for fulcio
+        enabled: false
+      args:
+        # TODO: Update once we have an intermediate cert
+        certificateAuthority: googleca
+        gcp_private_ca_parent: projects/oci-signer-service-dev/locations/us-central1/caPools/sigstore-ca
+      resources:
+        requests:
+          memory: "1G"
+          cpu: ".5"
+      serviceAccount:
+        annotations:
+          iam.gke.io/gcp-service-account: sigstore-prod-fulcio-sa@oci-signer-service-dev.iam.gserviceaccount.com
+      securityContext:
+        runAsNonRoot: true
+        runAsUser: 65533
+      # TODO: Remove once we aren't using a LoadBalancer
+      service:
+        type: LoadBalancer
+        ports:
+          - name: 5555-tcp
+            port: 80
+            protocol: TCP
+            targetPort: 5555
+          - name: 2112-tcp
+            port: 2112
+            protocol: TCP
+            targetPort: 2112
+    EOF
+  ]
+}
