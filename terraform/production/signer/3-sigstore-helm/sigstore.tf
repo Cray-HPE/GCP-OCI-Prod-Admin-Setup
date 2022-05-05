be459144908dcbbd3b4af2ea3ad620e256384c3e
Priya Wadhwa <priya@chainguard.dev>
2022-05-05 14:33:56 -0700

    Add kubectl provider to terraform

--- terraform/production/signer/3-sigstore-helm/sigstore.tf
+++ terraform/production/signer/3-sigstore-helm/sigstore.tf
@@ -1,30 +1,3 @@
-data "google_client_config" "current" {
-}
-
-data "google_container_cluster" "sigstore_prod" {
-  name     = var.cluster_name
-  project  = var.project_id
-  location = var.region
-}
-
-provider "kubernetes" {
-  host                   = "https://${data.google_container_cluster.sigstore_prod.endpoint}"
-  cluster_ca_certificate = base64decode(data.google_container_cluster.sigstore_prod.master_auth.0.cluster_ca_certificate)
-  token                  = data.google_client_config.current.access_token
-  proxy_url              = "socks5://localhost:8118"
-}
-
-provider "helm" {
-  kubernetes {
-    host  = data.google_container_cluster.sigstore_prod.endpoint
-    token = data.google_client_config.current.access_token
-    cluster_ca_certificate = base64decode(
-      data.google_container_cluster.sigstore_prod.master_auth[0].cluster_ca_certificate,
-    )
-    proxy_url = "socks5://localhost:8118"
-  }
-}
-
 resource "helm_release" "trillian" {
   name             = "trillian"
   repository       = "https://sigstore.github.io/helm-charts"
