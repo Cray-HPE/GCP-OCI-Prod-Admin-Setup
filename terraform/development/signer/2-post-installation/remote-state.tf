8ccf3083e1c9ae0640bc7bcba086559e9c7b0532
Priya Wadhwa <priya@chainguard.dev>
2022-06-01 10:53:49 +0100

    Migrate terraform state into development gcs dir

--- terraform/development/signer/2-post-installation/remote-state.tf
+++ terraform/development/signer/2-post-installation/remote-state.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     # Remote backend for tf state
     bucket = "oci-signer-service-dev-terraform-state"
-    prefix = "/terraform/production/2-post-installation/"
+    prefix = "/terraform/development/2-post-installation/"
   }
 }
