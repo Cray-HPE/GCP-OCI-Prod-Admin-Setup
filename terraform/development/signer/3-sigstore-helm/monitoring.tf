a65627c391bfc05f0fec5b4f33f38b42c0428b06
Priya Wadhwa <priya@chainguard.dev>
2022-06-03 12:46:41 +0100

    Add prober and prober alerts

--- terraform/development/signer/3-sigstore-helm/monitoring.tf
+++ terraform/development/signer/3-sigstore-helm/monitoring.tf
@@ -33,3 +33,56 @@ resource "helm_release" "prometheus" {
     EOF
   ]
 }
+
+// This helm chart deploys a prober to the GKE cluster
+// The prober is responsible for polling all Rekor and Fulcio endpoints
+// every 10 seconds. It reports latency, error code and endpoint.
+// This data is exported to Stackdriver via Prometheus, which is then used to create
+// alerts in GCP Monitoring.
+resource "helm_release" "sigstore_prober" {
+  name             = "sigstore-prober"
+  repository       = "https://sigstore.github.io/helm-charts"
+  chart            = "sigstore-prober"
+  namespace        = "sigstore-prober"
+  create_namespace = true
+  atomic           = true
+  version          = "0.0.1"
+  values = [
+    <<EOF
+    namespace:
+      create: false
+    serviceAccount:
+      create: true
+      name: sigstore-prober
+    spec:
+      replicaCount: 1
+      matchLabels:
+        app: sigstore-prober
+      args:
+        fulcioHost: http://fulcio-server.fulcio-system.svc
+        rekorHost: http://rekor-server.rekor-system.svc
+        frequency: 10
+    prometheus:
+      port: 8080
+    EOF
+  ]
+
+  depends_on = [
+    helm_release.prometheus
+  ]
+}
+
+// Monitoring
+module "monitoring" {
+  source = "../../modules/monitoring"
+
+  project_id = var.project_id
+
+  rekor_url               = "rekor-server.rekor-system.svc"
+  fulcio_url              = "fulcio-server.fulcio-system.svc"
+  notification_channel_id = "3677712677944843815"
+
+  depends_on = [
+    helm_release.sigstore_prober,
+  ]
+}
